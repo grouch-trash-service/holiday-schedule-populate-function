fcfe6eb2de4711b2bb4326d34700bf4d241604d2
Matt Porter <matthew.js.porter@gmail.com>
2020-10-20 19:07:45 -0400

    adding lambda.py module

---
+++ holiday/lambda.py
@@ -0,0 +1,13 @@
+"""
+lambda module for invoking lambda function to populate holiday schedule for trash pickup.
+"""
+
+
+def lambda_handler(event, context):
+    """
+    lambda function for populating trash holiday schedule
+    :param event: lambda event
+    :param context: lambda context
+    """
+    print(event)
+    print(context)
