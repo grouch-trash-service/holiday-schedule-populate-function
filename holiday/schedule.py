97d02d08b99ba5e793033432291cfe98e0b5f844
Matt Porter <matthew.js.porter@gmail.com>
2020-10-26 00:43:51 -0400

    adding schedule.py for interacting with trash holiday schedule service

---
+++ holiday/schedule.py
@@ -0,0 +1,54 @@
+"""
+Module for updating trash holiday schedule
+"""
+
+from urllib.parse import urlparse
+
+import boto3
+import requests
+from aws_requests_auth.aws_auth import AWSRequestsAuth
+from botocore.credentials import Credentials
+
+
+class TrashScheduleService:
+    """
+    TrashScheduleService is a service class that updates the trash holiday schedule
+    """
+
+    def __init__(self, url: str):
+        self.url = url
+
+    @staticmethod
+    def __get_credentials() -> Credentials:
+        session = boto3.Session()
+        credentials = session.get_credentials()
+        return credentials.get_frozen_credentials()
+
+    def _get_auth(self) -> AWSRequestsAuth:
+        host = urlparse(self.url).netloc
+        credentials = TrashScheduleService.__get_credentials()
+        return AWSRequestsAuth(aws_access_key=credentials.access_key,
+                               aws_secret_access_key=credentials.secret_key,
+                               aws_token=credentials.token,
+                               aws_host=host,
+                               aws_region=boto3.Session().region_name,
+                               aws_service='execute-api')
+
+    def update(self, holiday: dict):
+        """
+        updates holiday
+        :param holiday: dict containing name of holiday and route delays
+        """
+        auth = self._get_auth()
+        data = {
+            'data': holiday
+        }
+        requests.put(self.url+"/{name}".format(name=holiday['name']),json=data, auth=auth)
+
+    def delete(self, holiday: str):
+        """
+        deletes holiday
+        :param holiday: name of the holiday to delete
+        """
+        auth = self._get_auth()
+        requests.delete(self.url+f"/{holiday}", auth=auth)
---
+++ tests/test_schedule.py
@@ -0,0 +1,60 @@
+import unittest
+import boto3
+import requests
+from unittest.mock import patch, MagicMock
+
+from aws_requests_auth.aws_auth import AWSRequestsAuth
+from botocore.credentials import Credentials
+
+from holiday.schedule import TrashScheduleService
+
+
+class TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.trash_schedule_service = TrashScheduleService('https://test.amazonaws.com/Prod/v1/holidays')
+        self.auth = AWSRequestsAuth(aws_access_key='ACCESS_KEY',
+                                    aws_secret_access_key='SECRET',
+                                    aws_host='test.amazonaws.com',
+                                    aws_region='us-east1',
+                                    aws_service='execute-api')
+
+
+    @patch('boto3.Session')
+    def test_get_auth(self, test_patch):
+        expected_credentials = Credentials('ACCESS_KEY', 'SECRET')
+        session = TestCase.__mock_boto3_session(expected_credentials)
+        test_patch.return_value = session
+
+        expected_auth = self.auth
+
+        auth = self.trash_schedule_service._get_auth()
+
+        self.assertEqual(expected_auth.aws_access_key, auth.aws_access_key)
+        self.assertEqual(expected_auth.aws_secret_access_key, auth.aws_secret_access_key)
+        self.assertEqual(expected_auth.aws_host, auth.aws_host)
+        self.assertEqual(expected_auth.aws_region, auth.aws_region)
+
+    @patch('requests.put')
+    def test_update(self, mock):
+        holiday = {'name': 'Christmas', 'routeDelays': 'No Delays'}
+        self.trash_schedule_service.update(holiday)
+        self.assertTrue(mock.called)
+
+    @patch('requests.delete')
+    def test_update(self, mock):
+        holiday = 'Christmas'
+        self.trash_schedule_service.delete(holiday)
+        self.assertTrue(mock.called)
+
+    @staticmethod
+    def __mock_boto3_session(credentials):
+        session = boto3.Session()
+        credentials.get_frozen_credentials = MagicMock(return_value=credentials)
+        session.get_credentials = MagicMock(return_value=credentials)
+        session.region_name = 'us-east1'
+        return session
+
+
+if __name__ == '__main__':
+    unittest.main()
